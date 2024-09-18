75abb8237c0d9b2dc4398a72470f1e8236f8f452
Douglas Vieira <nck77630@gmail.com>
2024-09-18 16:55:47 -0300

    Correção do exercício

--- main.js
+++ main.js
@@ -1,38 +1,24 @@
-function Aluno(nome) {
+function Pessoa(nome) {
     this.nome = nome;
-    this.nomeAluno = function () {
-        console.log(`O aluno: ${this.nome}`);
-    }
 }
 
-function Turma(nome, notaA) {
-    this.nome = nome;
-    let _notaA = notaA;
+function Carro(modelo, fabricante, anoModelo, anoFabricacao, nome) {
+    this.modelo = modelo;
+    this.fabricante = fabricante;
+    this.anoModelo = anoModelo;
+    this.anoFabricacao = anoFabricacao;
 
-    // getters e setters
-    this.getNota = function () {
-        return _notaA;
-    }
-
-    this.setNota = function (valor) {
-        if (typeof valor === 'number') {
-            _notaA = valor;
-        }
-    }
-
-    //polimorfismo
-    this.media = function () {
-        const novoNota = _notaA / 2;
-        _notaA = novoNota;
-    }
-    //fim polimorfismo
-
-    // herança
-    Aluno.call(this, nome);
+    Pessoa.call(this, nome);
 }
 
-const aluno01 = new Turma('Maria', 10);
+function outraMarca(nome) {
+    Carro.call(this, 'Prisma', 'Chevrolet', 2021, 2020, nome);
+}
 
-aluno01.media();
-console.log(aluno01.nomeAluno());
-console.log(`Tem uma média de: ${aluno01.getNota()} pontos.`);
+const carroDoJoao = new Carro('Fiesta', 'Ford', 2020, 2019, 'João');
+const carroDaMaria = new Carro('Ka', 'Ford', 2021, 2020, 'Maria');
+const carroDaPedro = new outraMarca('Pedro');
+
+console.log(carroDoJoao);
+console.log(carroDaMaria);
+console.log(carroDaPedro);
