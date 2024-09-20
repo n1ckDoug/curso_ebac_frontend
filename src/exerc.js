541f59581320ecb363d58c3f4e74e89db146517e
Douglas Vieira <nck77630@gmail.com>
2024-09-20 15:10:54 -0300

    Exercício TypeScript

---
+++ src/exerc.js
@@ -0,0 +1,8 @@
+"use strict";
+const mult = (b, a) => b * a;
+console.log(mult(20, 10));
+const nome = 'Douglas';
+function dizOi() {
+    return `Olá ${nome}`;
+}
+console.log(dizOi());
---
+++ src/exerc.ts
@@ -0,0 +1,9 @@
+const mult = (b: number, a: number): number => b * a;
+console.log(mult(20, 10));
+
+const nome: string = 'Douglas';
+function dizOi(): string {
+    return `Olá ${nome}`;
+}
+
+console.log(dizOi());
